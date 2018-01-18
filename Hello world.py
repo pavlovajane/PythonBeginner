9eb9e6ee9c8c0bd5667550ef664db1d1aa52e55d
janepavlova <pavlova.jane@gmail.com>
2018-01-18 23:07:00 +0300

    First commit

--- Hello world.py
+++ Hello world.py
@@ -1,7 +1,9 @@
 # greeting = "Hello"
 # name = input("Please enter your name: ")
 # print(greeting + ", " + name + "!")
-anotherSplitString = """This string has been split 
-over
-several lines"""
-print(anotherSplitString)
+# anotherSplitString = """This string has been split
+# over
+# several lines"""
+# print(anotherSplitString)
+#
+# print('''The pet owner said "No, no, e's' uh...he's resting"''')
