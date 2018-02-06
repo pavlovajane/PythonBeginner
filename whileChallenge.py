5ad4468f18d242535eea9e9f20e8cb3a8a62d0d1
janepavlova <pavlova.jane@gmail.com>
2018-02-06 20:46:31 +0300

    While challenge

---
+++ whileChallenge.py
@@ -0,0 +1,20 @@
+
+import random
+
+highest = 10
+answer = random.randint(1, highest)
+
+print("Please guess a number between 1 and {}: ".format(highest))
+guess = int(input())
+if guess != answer:
+    if guess < answer:
+        print("Please guess higher")
+    else:  # guess must be greater than number
+        print("Please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it")
+    else:
+        print("Sorry, you have not guessed correctly")
+else:
+    print("You got it first time")
