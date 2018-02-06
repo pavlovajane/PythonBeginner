76edfcd43ebb5fc4fa1e17012591a7f00f12cbcb
janepavlova <pavlova.jane@gmail.com>
2018-02-06 21:45:58 +0300

    While challenge

--- whileChallenge.py
+++ whileChallenge.py
@@ -8,15 +8,14 @@ print("Please guess a number between 1 and {}: ".format(highest))
 guess = 0
 while guess != answer:
     guess = int(input())
-    if guess < answer:
+    if guess == 0:
+        print("Fire escape")
+        break
+    elif guess < answer:
         print("Please guess higher")
     elif guess > answer:  # guess must be greater than number
         print("Please guess lower")
 
     if guess == answer:
         print("Well done, you guessed it")
-    elif guess == 0:
-        print("Fire escape")
         break
-    # else:
-    #     print("Sorry, you have not guessed correctly")
