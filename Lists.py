811aa4239a0773bed68c494ee88f2ef32b111a5c
pavlovajane <pavlova.jane@gmail.com>
2018-02-07 12:49:48 +0300

    Working with lists/tuples/arrays

---
+++ Lists.py
@@ -0,0 +1,14 @@
+
+parrot_list = ["non pinin", "no more", "a stiff", "berefit of live"]
+
+parrot_list.append("Norwegian Blue")
+for state in parrot_list:
+    print("This parrot is "+state)
+
+even = [1,2,3,4]
+odd = [1,3,4,4,4]
+numbers = even + odd
+print(numbers)
+
+sortedNumbers = sorted(numbers)
+print(sortedNumbers)
